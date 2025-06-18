590dfc00d0ba3ec4a303abc705a1a55aaea3c1ac
Rahulpreet7 <rahulpre@ualberta.ca>
2025-06-18 09:33:47 -0600

    added requirements and setup; added all code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Informtion Retrieval Project',
+    version= '0.0.0',
+    author= 'Rahulpreet',
+    author_email= 'engg.rahulpreet@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
